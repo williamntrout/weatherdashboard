a6aa1568e41818084889e8e5e6a6b5ca2e5d0a3e
William Trout <troutw5593@gmail.com>
2020-04-11 09:29:01 -0500

    adding some basic elements and notes to get started.

---
+++ script.js
@@ -0,0 +1,6 @@
+// API key
+var apiKey = 'AIzaSyCTncm2eSA8wKKwAbMxC_Du3JkTeig0huE';
+// pull current date from moment
+var today = moment().format('DD/MM/YYYY');
+// city search array
+var city = [];
